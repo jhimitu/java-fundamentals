90b34a5b12e5401f8e532f96ea6ce7197d674c9b
Jhia Turner <jhimitu@gmail.com>
2019-05-28 10:58:21 -0700

    implement flipNHeads function

--- basics/Main.java
+++ basics/Main.java
@@ -1,7 +1,11 @@
+import java.util.*;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("java working");
     System.out.println(pluralize("dog", 2));
+
+    flipNHeads(1);
   }
 
   public static String pluralize(String word, int num) {
@@ -9,5 +13,30 @@ public class Main {
       return word + "s";
     } 
     return word;
+  }
+
+  public static void flipNHeads(int n) {
+    int flips = 0;
+    int heads = 0;
+    String lastFlip = "";
+    
+    while (flips < n) {
+      double randomNum = Math.random();
+      if (randomNum > .5) {
+        System.out.println("heads");
+        lastFlip = "heads";
+        if (lastFlip == "heads") {
+          heads++;
+        }
+      } else if (randomNum < .5) {
+        System.out.println("tails");
+        lastFlip = "tails";
+      }
+      flips++;
+    }
+    
+    if(heads == n) {
+      System.out.println("It took " + flips + " flips to flip " + n + " heads in a row.");
+    }
   }
 }
