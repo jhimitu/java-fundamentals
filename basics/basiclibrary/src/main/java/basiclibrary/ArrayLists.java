ceb3889450f62472d110743ce57aed0c0d52952f
Jhia Turner <jhimitu@gmail.com>
2019-05-29 13:05:06 -0700

    set up functions and tests to run correctly

---
+++ basics/basiclibrary/src/main/java/basiclibrary/ArrayLists.java
@@ -0,0 +1,23 @@
+package basiclibrary;
+
+public class ArrayLists {
+    public static int[] roll(int n) {
+        int[] result = new int[n];
+        return result;
+    }
+
+    public static boolean containsDuplicates() {
+        return true;
+    }
+
+    public static int calculateAverage(int[] inputArr) {
+        int average = 0;
+        return average;
+    }
+
+    public static int[] arrOfArrAverage(int[][] inputArr) {
+        int[] result = inputArr[0];
+        return result;
+    }
+
+}
