9cfe393ebee4577d6fb1a459a22d713b39505d30
Jhia Turner <jhimitu@gmail.com>
2019-05-30 09:06:48 -0700

    implement roll method that passes test

--- basics/basiclibrary/src/main/java/basiclibrary/ArrayLists.java
+++ basics/basiclibrary/src/main/java/basiclibrary/ArrayLists.java
@@ -1,8 +1,16 @@
 package basiclibrary;
+import java.lang.Math;
 
 public class ArrayLists {
     public static int[] roll(int n) {
         int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            int max = 6;
+            int min = 1;
+            int range = max - min + 1;
+            int dieRoll = (int) (Math.random() * range) + min;
+            result[i] = dieRoll;
+        }
         return result;
     }
 
@@ -19,5 +27,4 @@ public class ArrayLists {
         int[] result = inputArr[0];
         return result;
     }
-
 }
