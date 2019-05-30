aad062f5f62bdfc513c370d0364e5d93d0b0c0c8
Jhia Turner <jhimitu@gmail.com>
2019-05-30 12:35:19 -0700

    set up linter and basic tally function stub

---
+++ basics/basiclibrary/src/main/java/basiclibrary/Tally.java
@@ -0,0 +1,10 @@
+package basiclibrary;
+
+import java.util.List;
+
+public class Tally {
+    public static String tally(List<String> list) {
+        String result = "";
+        return result;
+    }
+}
