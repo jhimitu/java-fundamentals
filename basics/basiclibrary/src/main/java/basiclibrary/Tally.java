19322618e7f1337684ece9bb0af269c38b6a3b34
Jhia Turner <jhimitu@gmail.com>
2019-05-30 22:51:28 -0700

    find the highest and lowest temps

--- basics/basiclibrary/src/main/java/basiclibrary/Tally.java
+++ basics/basiclibrary/src/main/java/basiclibrary/Tally.java
@@ -1,8 +1,62 @@
 package basiclibrary;
 
 import java.util.List;
+import java.util.*;
 
 public class Tally {
+    public static String maps(int[][] inputArr) {
+        String result = "";
+        HashSet<Integer> temperatures = new HashSet<Integer>();
+
+        //store high temp
+        int highTemp;
+        int lowTemp;
+
+        for (int[] arr : inputArr) {
+            for (int i = 0; i < arr.length; i++) {
+                temperatures.add(arr[i]);
+            }
+        }
+        System.out.println(temperatures);
+
+        //find the highest temperature
+        Iterator highTemps = temperatures.iterator();
+        highTemp = (int) highTemps.next();
+
+        while (highTemps.hasNext()) {
+            int current = (int) highTemps.next();
+
+            if (current > highTemp) {
+                highTemp = current;
+            }
+        }
+
+        //find the lowest temperature
+        Iterator lowTemps = temperatures.iterator();
+        lowTemp = (int) lowTemps.next();
+
+        while (lowTemps.hasNext()) {
+            int current = (int) lowTemps.next();
+
+            if (current < lowTemp) {
+                lowTemp = current;
+            }
+        }
+
+        result = "High: " + highTemp + "\n"
+                + "Low: " + lowTemp;
+
+        for (int i = lowTemp; i < highTemp; i++) {
+            //if temperatures does not contain the temp at i
+            //list as 'never seen'
+        }
+
+        System.out.println(highTemp);
+        System.out.println(lowTemp);
+        System.out.println(result);
+        return result;
+    }
+
     public static String tally(List<String> list) {
         String result = "";
         return result;
--- basics/basiclibrary/src/test/java/basiclibrary/LibraryTest.java
+++ basics/basiclibrary/src/test/java/basiclibrary/LibraryTest.java
@@ -43,4 +43,24 @@ public class LibraryTest {
         int[] expected = input[2];
         assertEquals("should return array with the lowest average", expected, ArrayLists.arrOfArrAverage(input));
     }
+
+    @Test public void testMaps() {
+        int [][] input = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+        String expected = "High: 72\n" +
+                "Low: 51\n" +
+                "Never saw temperature: 63\n" +
+                "Never saw temperature: 67\n" +
+                "Never saw temperature: 68\n" +
+                "Never saw temperature: 69";
+        assertEquals("should return a string", expected, Tally.maps(input));
+    }
+
+    @Test public void testTally() {
+
+    }
 }
